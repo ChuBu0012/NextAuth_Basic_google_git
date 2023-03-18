2a4f78f2de863d7d3eac293f4d9467c0b54e15df
Mostqto <moomost123@gmail.com>
2023-03-19 01:18:12 +0700

    four

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,7 +3,7 @@ import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from 'next-auth/providers/github';
 
 export default NextAuth({
-    providers : [
+    providers: [
         // Google Provider
         GoogleProvider({
             clientId: process.env.NEXT_PUBLIC_GOOGLE_ID,
@@ -13,5 +13,6 @@ export default NextAuth({
             clientId: process.env.NEXT_PUBLIC_GITHUB_ID,
             clientSecret: process.env.NEXT_PUBLIC_GITHUB_SECRET
         })
-    ]
+    ],
+    secret: "yH+sOgef5JvuB6TAQYfU535CMS4u99E8KV51/fdAGw8="
 })
